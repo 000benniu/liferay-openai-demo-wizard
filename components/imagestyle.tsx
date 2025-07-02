9c100fbedcd4e566ed291ee4359f7beeb7824d59
Ben Niu <Ben67366@gmail.com>
2025-07-02 11:21:41 +0900

    change the image back to img

--- components/imagestyle.tsx
+++ components/imagestyle.tsx
@@ -1,6 +1,5 @@
 import { BookOpenIcon, XCircleIcon } from '@heroicons/react/24/solid';
 import { useEffect, useState } from 'react';
-import Image from 'next/image';
 import React from 'react';
 
 export default function AppImageStyle({ styleInputChange }) {
@@ -89,7 +88,7 @@ export default function AppImageStyle({ styleInputChange }) {
                               setStyleChoice(formatStyleName(value))
                             }
                           >
-                            <Image
+                            <img
                               className='border-2 border-blue-300 hover:border-blue-600 cursor-pointer shadow-md'
                               src={'/images/art-styles/' + value}
                             />
